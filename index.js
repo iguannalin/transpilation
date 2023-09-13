db94b64975828b0ec1b0edcf34f8f873c7b20b26
seasons986 <annalin12345@gmail.com>
2023-09-12 23:11:54 -0400

    findIndex was too slow; optimized json file for lookup instead

--- index.js
+++ index.js
@@ -1,35 +1,31 @@
 window.addEventListener("load", () => {
-  function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min);
-  }
-  
   const container = document.getElementById("container");
   let alphabet;
 
   function onHover(e) {
-    const found = Object.values(alphabet).indexOf(e.target.innerHTML);
-    if (found) {
-      console.log({found});
-      e.target.innerHTML = Object.keys(alphabet)[found];
-    }
+    console.log(e.target.innerText)
+    e.target.innerHTML = alphabet[e.target.innerText]
     e.target.removeEventListener("touchstart", onHover);
     e.target.removeEventListener("mouseover", onHover);
+  }
+
+  function displayChar(ch, canHover = false) {
+    if (!ch) return;
+    const text = document.createElement("span");
+    text.innerHTML = ch;
+    if (canHover) {
+      text.addEventListener("touchstart", onHover);
+      text.addEventListener("mouseover", onHover);
+    }
+    container.appendChild(text);
   }
 
   function display(ww) {
     ww.split("").forEach((ch) => {
       ch = ch.toLowerCase();
-      if (ch == "*") container.innerHTML += "&nbsp;";
-      else if (!alphabet[ch]) container.innerHTML += ch;
-      else {
-        const text = document.createElement("span");
-        text.innerHTML = alphabet[ch];
-        text.addEventListener("touchstart", onHover);
-        text.addEventListener("mouseover", onHover);
-        container.appendChild(text);
-      }
+      if (ch == "*") displayChar("&nbsp;");
+      else if (!alphabet[ch]) displayChar(ch);
+      else displayChar(alphabet[ch], true);
     })
   }
 
